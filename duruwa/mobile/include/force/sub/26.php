812b80dac5199faacf460530866522a080c0f1bf
Byung Hoon Lim <>
2020-06-24 15:16:06 +0900

    처리

--- duruwa/mobile/include/force/sub/26.php
+++ duruwa/mobile/include/force/sub/26.php
@@ -3,139 +3,269 @@
 
 
 <div class="s20_btn">
-	<a href="#" class="on20">속눈썹</a>
-	<a href="#">왁싱</a>
-	<a href="#">강사트레이닝</a>
+	<a href="#" class="on20">뷰티 콘텐츠 촬영</a>
+	<a href="#">뷰티 일러스트레이터</a>
+	<a href="#">뷰티 그래픽 디자인</a>
+	<a href="#">뷰티 프리미어 에디트 디자인</a>
 </div>
 
 
 
-	<div class="s20">
-		<img src="<?=$head_skin_url?>/img/s26.jpg">
-		<div class="s20_common s26">
-			<div>
-				<h1>Eyelash</h1>
-				<p>전문 미용사가 되기 위한 교육으로 헤어반, 자격증 시험 대비반으로 나누어 체계적인 교육을 실시합니다. 기성 미용인의 재교육을 위한 수시 강좌도 개설하여, 발전하는 미용인을 양성하는데 역점을 두고 있습니다. 또한 자기연출의 기초가 되는 헤어에 관한 전문 헤어 교육의 이론과 실제를 학습 연구하고 나아가 각 분야에서 보다 전문적인 지식과 기술을 펼칠 유능한 전문 헤어 디자이너를 양성합니다.</p>
-			</div>
+<div class="s20">
+	<img src="<?= $head_skin_url ?>/img/s26.jpg">
+	<div class="s20_common s26">
+		<div>
+			<h1>뷰티스마트AI</h1>
+			<p>현대 트랜드에 알맞게 IT기술을 이용한 교육도 실시하고 있습니다.</p>
 		</div>
-
-		<h2>속눈썹 디자이너</h2>
-		<div class="s20_lr">
-			<div class="s20_left"><img src="<?=G5_URL?>/img/s26_a.jpg"></div>
-			<div class="s20_right">
-				<p>속눈썹 연장 및 증모술을 배우는 것에 그치지 않고 트렌드나 고객의 요구에 맞는 속눈썹 디자인을 제시할 수 있는 속눈썹 디자이너 양성과정 입니다. 속눈썹 연장술의 이해와 특징, 연장술 및 증모술의 차이점을 학습하며 도구와 재료의 정확한 사용법을 이해하는 교육과정으로 특수모를 활용한 각각의 모를 연결하는 기법으로 속눈썹 익스텐션을 통해 풍성하고 긴 속눈썹을 연출할 수 있습니다.</p>
-				<div class="s21_tb_a">
-					<table>
-						<colgroup>
-							<col>
-							<col>
-							<col>
-							<col>
-							<col>
-						</colgroup>
-						<tr>
-							<th>과목</th>
-							<th>과정</th>
-							<th>수업시간</th>
-							<th>교육시간</th>
-							<th>수업횟수</th>
-						</tr>
-						<tr>
-							<td rowspan="3">속눈썹디자이너</td>
-							<td>정규반</td>
-							<td>1개월</td>
-							<td>3시간</td>
-							<td>주2회</td>
-						</tr>
-						<tr>
-							<td>실무반</td>
-							<td>2개월</td>
-							<td>3시간</td>
-							<td>주2회</td>
-						</tr>
-						<tr>
-							<td>강사트레이닝</td>
-							<td>1개월</td>
-							<td>3시간</td>
-							<td>주1회</td>
-						</tr>
-					</table>
-					<span class="ps_dec">* 일정에 따라 수업진행은 상이할 수 있습니다</span>
-				</div>
-			</div>
-		</div>
-
-		<div><img src="<?=G5_URL?>/img/s22_com.png"><br><br></div>
 	</div>
 
-	<div class="s20" style="display:none;">
-		<img src="<?=$head_skin_url?>/img/s262.jpg">
-		<div class="s20_common s262">
-			<div>
-				<h1>Waxing</h1>
-				<p>왁싱전문가가 되기 위한 왁싱 기초이론, 실무 바탕의 교육과정</p>
+	<h2>뷰티 콘텐츠 촬영</h2>
+	<div class="s20_lr">
+		<div class="s20_left"><img src="<?= G5_URL ?>/img/s26_c.jpg"></div>
+		<div class="s20_right">
+			<!-- <p>속눈썹 연장 및 증모술을 배우는 것에 그치지 않고 트렌드나 고객의 요구에 맞는 속눈썹 디자인을 제시할 수 있는 속눈썹 디자이너 양성과정 입니다. 속눈썹 연장술의 이해와 특징, 연장술 및 증모술의 차이점을 학습하며 도구와 재료의 정확한 사용법을 이해하는 교육과정으로 특수모를 활용한 각각의 모를 연결하는 기법으로 속눈썹 익스텐션을 통해 풍성하고 긴 속눈썹을 연출할 수 있습니다.</p> -->
+			<div class="s21_tb_a">
+				<table>
+					<colgroup>
+						<col>
+						<col>
+						<col>
+					</colgroup>
+					<tr>
+						<th>주차</th>
+						<th>1</th>
+						<th>2</th>
+					</tr>
+					<tr>
+						<td>1주차</td>
+						<td>뷰티 콘텐츠 들여다 보기</td>
+						<td>뷰티 콘텐츠 워크</td>
+					</tr>
+					<tr>
+						<td>2주차</td>
+						<td>예뻐보이는 빛 알아보기</td>
+						<td>뷰티 이미지 구성</td>
+					</tr>
+					<tr>
+						<td>3주차</td>
+						<td>장점 살리는 뷰티 구도</td>
+						<td>내 프로필 제작</td>
+					</tr>
+					<tr>
+						<td>4주차</td>
+						<td>화장품 홍보 포스터 제작</td>
+						<td>뷰티 화보 촬영</td>
+					</tr>
+				</table>
+				<span class="ps_dec">* 일정에 따라 수업진행은 상이할 수 있습니다</span>
 			</div>
 		</div>
+	</div>
+</div>
 
-		<h2>왁싱전문가</h2>
-		<div class="s20_lr">
-			<div class="s20_left"><img src="<?=G5_URL?>/img/s26_b.jpg"></div>
-			<div class="s20_right">
-				<p>현대사회는 노출의 기회가 많고, 많은 사람들은 자신의 피부를 아름답게 표현하는데 몸의 과다한 털과 필요 없는 부위의 털을 없애기를 원합니다. 왁스를 가열하여 액체 상태로 만든 후 적당히 식힌 후에 털이 많은 부위에 바르고, 시간이 적당히 흘러 왁스가 식고 굳은 후에 제거하는 방법을 이용하는 것이 왁싱 입니다. 다양한 왁싱 기술과 제품을 사용하여 고객에게 맞는 최상의 서비스를 제공해 줄 수 있는 왁싱전문가를 양성하는 교육과정 입니다.</p>
-				<div class="s21_tb_a">
-					<table>
-						<colgroup>
-							<col>
-							<col>
-							<col>
-							<col>
-							<col>
-							<col>
-						</colgroup>
-						<tr>
-							<th>과목</th>
-							<th>과정</th>
-							<th>세부과정</th>
-							<th>수업시간</th>
-							<th>교육시간</th>
-							<th>수업횟수</th>
-						</tr>
-						<tr>
-							<td rowspan="4">왁싱전문가</td>
-							<td>창업반</td>
-							<td>Fullbody 과정<br>Face과정<br>Bikiny과정</td>
-							<td>2개월</td>
-							<td>3시간</td>
-							<td>주2회</td>
-						</tr>
-						<tr>
-							<td rowspan="3">마스터반</td>
-							<td>Fullbody 과정</td>
-							<td>조율</td>
-							<td>3시간</td>
-							<td>주1회</td>
-						</tr>
-						<tr>
-							<td>Face과정</td>
-							<td>조율</td>
-							<td>3시간</td>
-							<td>주1회</td>
-						</tr>
-						<tr>
-							<td>Bikiny과정</td>
-							<td>조율</td>
-							<td>3시간</td>
-							<td>주1회</td>
-						</tr>
-					</table>
-					<span class="ps_dec">* 일정에 따라 수업진행은 상이할 수 있습니다</span>
-				</div>
+<div class="s20" style="display:none;">
+	<img src="<?= $head_skin_url ?>/img/s26.jpg">
+	<div class="s20_common s262">
+		<div>
+			<h1>뷰티스마트AI</h1>
+			<p>현대 트랜드에 알맞게 IT기술을 이용한 교육도 실시하고 있습니다.</p>
+		</div>
+	</div>
+
+	<h2>뷰티 일러스트레이터</h2>
+	<div class="s20_lr">
+		<div class="s20_left"><img src="<?= G5_URL ?>/img/s26_d.jpg"></div>
+		<div class="s20_right">
+			<!-- <p>현대사회는 노출의 기회가 많고, 많은 사람들은 자신의 피부를 아름답게 표현하는데 몸의 과다한 털과 필요 없는 부위의 털을 없애기를 원합니다. 왁스를 가열하여 액체 상태로 만든 후 적당히 식힌 후에 털이 많은 부위에 바르고, 시간이 적당히 흘러 왁스가 식고 굳은 후에 제거하는 방법을 이용하는 것이 왁싱 입니다. 다양한 왁싱 기술과 제품을 사용하여 고객에게 맞는 최상의 서비스를 제공해 줄 수 있는 왁싱전문가를 양성하는 교육과정 입니다.</p> -->
+			<div class="s21_tb_a">
+				<table>
+					<colgroup>
+						<col>
+						<col>
+						<col>
+					</colgroup>
+					<tr>
+						<th>주차</th>
+						<th>1</th>
+						<th>2</th>
+					</tr>
+					<tr>
+						<td>1주차</td>
+						<td>뷰티 일러스트 아이템 구상</td>
+						<td>뷰티 일러스트레이터 굿즈</td>
+					</tr>
+					<tr>
+						<td>2주차</td>
+						<td>개성있는 뷰티 이미지 만들기</td>
+						<td>귀여운 뷰티 캐릭터 만들기 1</td>
+					</tr>
+					<tr>
+						<td>3주차</td>
+						<td>섹시한 뷰티 캐릭터 만들기 2</td>
+						<td>뷰티 팝아트 표현</td>
+					</tr>
+					<tr>
+						<td>4주차</td>
+						<td>뷰티 이미지와 컬러 감각</td>
+						<td>뷰티 포스터 제작</td>
+					</tr>
+				</table>
+				<span class="ps_dec">* 일정에 따라 수업진행은 상이할 수 있습니다</span>
 			</div>
 		</div>
-
 	</div>
 
+</div>
 
-	<div class="s20" style="display:none;">
-		<? include($head_skin_path."/sub/20_training.php"); ?>
+
+<div class="s20" style="display:none;">
+	<img src="<?= $head_skin_url ?>/img/s26.jpg">
+	<div class="s20_common s262">
+		<div>
+			<h1>뷰티스마트AI</h1>
+			<p>현대 트랜드에 알맞게 IT기술을 이용한 교육도 실시하고 있습니다.</p>
+		</div>
 	</div>
+
+	<h2>뷰티 그래픽디자인</h2>
+	<div class="s20_lr">
+		<div class="s20_left"><img src="<?= G5_URL ?>/img/s26_e.jpg"></div>
+		<div class="s20_right">
+			<!-- <p>현대사회는 노출의 기회가 많고, 많은 사람들은 자신의 피부를 아름답게 표현하는데 몸의 과다한 털과 필요 없는 부위의 털을 없애기를 원합니다. 왁스를 가열하여 액체 상태로 만든 후 적당히 식힌 후에 털이 많은 부위에 바르고, 시간이 적당히 흘러 왁스가 식고 굳은 후에 제거하는 방법을 이용하는 것이 왁싱 입니다. 다양한 왁싱 기술과 제품을 사용하여 고객에게 맞는 최상의 서비스를 제공해 줄 수 있는 왁싱전문가를 양성하는 교육과정 입니다.</p> -->
+			<div class="s21_tb_a">
+				<table>
+					<colgroup>
+						<col>
+						<col>
+						<col>
+					</colgroup>
+					<tr>
+						<th>주차</th>
+						<th>1</th>
+						<th>2</th>
+					</tr>
+					<tr>
+						<td>1주차</td>
+						<td>
+							뷰티 디자인 커뮤니케이션<br>
+							1.포토샵 기초(이미지확장자,단축키,핵심툴 익히기)<br>
+							2.레이어,펜툴 연습
+						</td>
+						<td>
+							뷰티와 디지털의 관계<br>
+							1.배경 보정에 따른 전,후 비교(도장,패치툴)<br>
+							2.인물 보정에 따른 전,후 비교(픽셀유동화)
+						</td>
+					</tr>
+					<tr>
+						<td>2주차</td>
+						<td>
+							뷰티 이미지 창의적으로 생각하기<br>
+							1.이미지 색상변화로 분위기 바꿔보기<br>
+							2.이미지 추가와 제거로 분위기 바꿔보기
+						</td>
+						<td>
+							뷰티 아트 실무<br>
+							1.마스크 활용법<br>
+							2.블렌딩 효과
+						</td>
+					</tr>
+					<tr>
+						<td>3주차</td>
+						<td>
+							뷰티 구성 단계<br>
+							1.뷰티 이미지 자유자재로 컨트롤<br>
+							2.텍스트 변형 하기
+						</td>
+						<td>
+							뷰티 아트의 제작법<br>
+							1.창작과 모방의 차이점 토론<br>
+							2.저작권에 대해 알아보기
+						</td>
+					</tr>
+					<tr>
+						<td>4주차</td>
+						<td>
+							뷰티 광고 작품 만들기<br>
+							1.주제 선정, 자료 수집<br>
+							2.완성 작품 발표
+						</td>
+						<td>움직이는 뷰티 그래픽 만들기</td>
+					</tr>
+				</table>
+				<span class="ps_dec">* 일정에 따라 수업진행은 상이할 수 있습니다</span>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+
+<div class="s20" style="display:none;">
+	<img src="<?= $head_skin_url ?>/img/s26_f.jpg">
+	<div class="s20_common s262">
+		<div>
+			<h1>뷰티스마트AI</h1>
+			<p>현대 트랜드에 알맞게 IT기술을 이용한 교육도 실시하고 있습니다.</p>
+		</div>
+	</div>
+
+	<h2>뷰티 프리미어 에디트 디자인</h2>
+	<div class="s20_lr">
+		<div class="s20_left"><img src="<?= G5_URL ?>/img/s26_f.jpg"></div>
+		<div class="s20_right">
+			<!-- <p>현대사회는 노출의 기회가 많고, 많은 사람들은 자신의 피부를 아름답게 표현하는데 몸의 과다한 털과 필요 없는 부위의 털을 없애기를 원합니다. 왁스를 가열하여 액체 상태로 만든 후 적당히 식힌 후에 털이 많은 부위에 바르고, 시간이 적당히 흘러 왁스가 식고 굳은 후에 제거하는 방법을 이용하는 것이 왁싱 입니다. 다양한 왁싱 기술과 제품을 사용하여 고객에게 맞는 최상의 서비스를 제공해 줄 수 있는 왁싱전문가를 양성하는 교육과정 입니다.</p> -->
+			<div class="s21_tb_a">
+				<table>
+					<colgroup>
+						<col>
+						<col>
+						<col>
+					</colgroup>
+					<tr>
+						<th>주차</th>
+						<th>1</th>
+						<th>2</th>
+					</tr>
+					<tr>
+						<td>1주차</td>
+						<td>
+							Personal PR의 중요성
+						</td>
+						<td>
+							뷰티 소품를 이용한 개성 살리기
+						</td>
+					</tr>
+					<tr>
+						<td>2주차</td>
+						<td>
+							다양한 모션을 활용한 뷰티 디지털 알기
+						</td>
+						<td>
+							뷰티 이미지 컨트롤
+						</td>
+					</tr>
+					<tr>
+						<td>3주차</td>
+						<td>
+							뷰티 디지털 이미지 만들기
+						</td>
+						<td>
+							뷰티인트로영상 만들기
+						</td>
+					</tr>
+					<tr>
+						<td>4주차</td>
+						<td>
+							연예인영상 제작
+						</td>
+						<td>뷰티 PR 파일 만들기</td>
+					</tr>
+				</table>
+				<span class="ps_dec">* 일정에 따라 수업진행은 상이할 수 있습니다</span>
+			</div>
+		</div>
+	</div>
+
+</div>
